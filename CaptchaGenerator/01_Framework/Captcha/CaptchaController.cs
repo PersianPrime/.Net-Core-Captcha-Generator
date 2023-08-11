8b08eac861cbcbf34c32b352b4656256345af484
persianprime <persianprime@gmail.com>
2023-08-11 11:26:43 +0330

    SomeChanges

---
+++ CaptchaGenerator/01_Framework/Captcha/CaptchaController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CaptchaGenerator.api
+{
+    public class CaptchaController : Controller
+    {
+      
+        //[Route("/[controller]")]
+        [Route("/get-captcha-image")]
+        public IActionResult OnGet()
+        {
+            int width = 100;
+            int height = 36;
+            var captchaCode = Captcha.GenerateCaptchaCode();
+            var result = Captcha.GenerateCaptchaImage(width, height, captchaCode);
+            HttpContext.Session.SetString("CaptchaCode", result.CaptchaCode);
+            //Stream s = new MemoryStream(result.CaptchaByteData);
+            return Json(result.CaptchBase64Data);
+            //return new FileStreamResult(s, "image/png");
+        }
+    }
+}
